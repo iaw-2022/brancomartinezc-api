f1a2ecb1da20ce6713b3ff062942d8216a969c19
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-04-28 19:32:33 -0300

    response con 404

--- src/controllers/cities.controller.js
+++ src/controllers/cities.controller.js
@@ -2,17 +2,32 @@ const db = require('../database');
 
 const getCities = async (req, res) => {
     const response = await db.query('SELECT * FROM cities');
-    res.status(200).json(response.rows);
+    
+    if(response.rows.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 const getCityById = async (req, res) => {
     const response = await db.query('SELECT * FROM cities WHERE id = $1',[req.params.id]);
-    res.status(200).json(response.rows);
+
+    if(response.rows.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 const getCitiesByCountry = async (req, res) => {
     const response = await db.query('SELECT * FROM cities WHERE country_code = $1',[req.params.id]);
-    res.status(200).json(response.rows);
+
+    if(response.rows.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 module.exports = {
--- src/controllers/properties.controller.js
+++ src/controllers/properties.controller.js
@@ -3,27 +3,52 @@ const getSQLQueryWithCombinedFilters = require('../utils/propertiesFilter');
 
 const getProperties = async (req, res) => {
     const response = await db.query(getSQLQueryWithCombinedFilters(req));
-    res.status(200).json(response.rows);
+    
+    if(response.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 const getPropertyById = async (req, res) => {
     const response = await db.query('SELECT * FROM properties WHERE id = $1',[req.params.id]);
-    res.status(200).json(response.rows);
+    
+    if(response.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 const getPropertiesByCity = async (req, res) => {
     const response = await db.query('SELECT * FROM properties WHERE city_id = $1',[req.params.id]);
-    res.status(200).json(response.rows);
+    
+    if(response.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 const getPropertiesByCountry = async (req, res) => {
     const response = await db.query('SELECT properties.* FROM properties INNER JOIN cities ON properties.city_id=cities.id WHERE cities.country_code=$1',[req.params.id]);
-    res.status(200).json(response.rows);
+    
+    if(response.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 const getPropertyPhotos = async (req, res) => {
     const response = await db.query('SELECT path FROM photos WHERE property_id = $1',[req.params.id]);
-    res.status(200).json(response.rows);
+    
+    if(response.length > 0){
+        res.status(200).json(response.rows);
+    }else{
+        res.status(404).json({error: 'not found'});
+    }
 };
 
 module.exports = {
